ff3d9ed6adeb66fae18d81f5302d322cf963dbdc
TipeSor <antanas8899@gmail.com>
2025-02-24 16:24:57 +0200

    added basic dragging logic

--- Program.cs
+++ Program.cs
@@ -6,13 +6,33 @@ namespace TipeEngine
 {
     class Program
     {
-        static List<TipeObject> objects =  new List<TipeObject>();
+        static List<TipeObject> objects = new List<TipeObject>();
+
+        static TipeObject? currentHeld;
 
         static void Main(string[] args)
         {
             RenderWindow window = new RenderWindow(new VideoMode(800, 600), "TipeEngine");
+            window.SetFramerateLimit(60);
 
             window.Closed += (sender, e) => window.Close();
+            window.MouseButtonPressed += (sender, e) =>
+            {
+                if (e.Button == Mouse.Button.Left)
+                {
+                    Console.WriteLine("Clicked Left");
+                    if (currentHeld == null)
+                    {
+                        Vector2f mousePosition = window.MapPixelToCoords(Mouse.GetPosition(window));
+                        currentHeld = objects.FirstOrDefault(o => o.GetGlobalBounds().Contains(mousePosition.X, mousePosition.Y));
+                        
+                    }
+                    else
+                    {
+                        currentHeld = null;
+                    }
+                }
+            };
 
             Texture texture = new Texture("Images/card-back.png");
 
@@ -41,6 +61,8 @@ namespace TipeEngine
                 window.DispatchEvents();
 
                 window.Clear(new Color(10, 10, 10, 255));
+
+                if (currentHeld != null) currentHeld.Position = window.MapPixelToCoords(Mouse.GetPosition(window));
 
                 objects.ForEach(o => window.Draw(o));
 
